c9ca7fdaed5cbcbfb36355aba2d25319e7129da1
Mon Maldonado <monterdi@gmail.com>
2019-02-01 13:38:36 +0100

    Class Date grafica

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from tkinter import *
 from tkinter import ttk
+from datetime import datetime
+
+class Date(ttk.Frame):
+    date = None
+    active = True
+    weekend = False
+    __lblDate = None
+
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, width=76, height=61, borderwidth=0.5, relief='groove')
+        self.__lblDate = ttk.Label(self, text="28", font=('Arial', 28, 'bold'), width=2, anchor=E)
+        self.__lblDate.place(x=37, y=22)
+
 
 class Calendar(ttk.Frame):
 
@@ -9,18 +22,22 @@ class Calendar(ttk.Frame):
 
         ttk.Frame.__init__(self, parent, width=self.__width, height=self.__height)
         self.__createHeader()
+        self.__createDaysNames()
 
+        for f in range(6):
+            for c in range(7):
+                Date(self).place(x=c*76, y=f*61+55)
+
+    def __createDaysNames(self):
         dias = ('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo')
         for i in range(7):
             f = ttk.Frame(self, width=76, height=14)
             f.pack_propagate(0)
-            ttk.Label(f, text=dias[i], font=('Arial', 11), anchor=CENTER, borderwidth=1, relief='solid').pack(fill=BOTH, expand=1)
+            ttk.Label(f, text=dias[i], font=('Arial', 11), anchor=CENTER, borderwidth=0.5, relief='groove').pack(fill=BOTH, expand=1)
             f.place(x=i*76, y=40)
 
-
-
     def __createHeader(self):
-        self.header = ttk.Frame(self, width=self.__width, height=40)
+        self.header = ttk.Frame(self, width=self.__width, height=40, borderwidth=0.5, relief='groove')
         self.header.place(x=0, y=0)
 
         self.__btnLastYear = ttk.Button(self.header, text="<<", width=2)
@@ -33,6 +50,8 @@ class Calendar(ttk.Frame):
         self.__btnNextMonth.place(x=408, y=8)
         self.__btnLastYear = ttk.Button(self.header, text=">>", width=2)
         self.__btnLastYear.place(x=462, y=8)
+
+    
 
 
 class MainApp(Tk):
